bbcf6d10963c60aca950807abfb6cb5cf8297928
Ajay Mathew <ajayedathazhe@gmail.com>
2025-08-27 09:48:58 +1000

    chore: upgrade node.js to v22.14.0

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import "./style.scss";
 
-import useFetch from "../../../hooks/UseFetch"
+import useFetch from "../../../hooks/UseFetch";
 
 import Img from "../../../components/lazyLoadingImage/Img";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
@@ -12,12 +12,12 @@ const HeroBanner = () => {
   const [background, setBackground] = useState("");
   const [query, setQuery] = useState("");
   const navigate = useNavigate();
-  const {url}  = useSelector((state) => state.home);
+  const { url } = useSelector((state) => state.home);
   console.log(url);
   const { data, loading } = useFetch("/movie/upcoming");
 
   useEffect(() => {
-    const bg = 
+    const bg =
       url.backdrop +
       data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
     setBackground(bg);
